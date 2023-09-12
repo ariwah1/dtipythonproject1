8e744b716d6708994690a1ab49fde534fef586ad
ariwah1 <s6652410013@sau.ac.th>
2023-09-12 08:39:01 +0700

    dtipython07.py

---
+++ lecture/lecture5/dtipython05.py
@@ -0,0 +1,16 @@
+#คำนวณงินที่จะเเชร์กัน ป้อนเงิน ป้อนคน เเล้วคำนวณเเละเเสดงเงินที่จะเเชร์กัน
+#โดยให้เขียนเป็นฟังชันก์ ขอ 2 ฟังชันก์
+def inputpersonmoney() :
+    Money = float( input('Money:'))
+    Person = int( input('Person:'))
+    return Money, Person 
+    
+def calinputSharemoney(money, person) :
+    M = format(money,".2f")
+    print(f'จำนวนเงิน {money:.2f} บาท  {person} คน เเชร์กันคนละ {round(money/person)} บาท')
+    print("จำนวนเงิน",("%.2f"%money),"บาท",(person),"คน เเชร์คนละ",round(money/person),"บาท")
+    print("จำนวนเงิน"+str("%.2f"%money)+'บาท '+str(person)+'คน เเชร์คนละ '+str(round(money/person)))
+    print('จำนวนเงิน {:.2f} บาท  {} คน เเชร์กันคนละ {} บาท'.format(money,person, round(money/person)))
+    print('จำนวนเงิน %s บาท %d คน เเชร์กันคนละ %d บาท'.format(M,person, round(money/person)))    
+Money,Person = inputpersonmoney()
+calinputSharemoney(Money,Person)
