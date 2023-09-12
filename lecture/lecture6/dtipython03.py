1f68cc5bc464f855fdb8d42005008380051d7204
ariwah1 <s6652410013@sau.ac.th>
2023-09-12 10:52:16 +0700

    Revert "Delete dtipython03.py"
    This reverts commit fac38ff6ed768b0ea307d1f6eb73d6063e71f1d2.

---
+++ lecture/lecture6/dtipython03.py
@@ -0,0 +1,12 @@
+# คำสั่ง if-else
+
+if 10 > 20 ** 2 :
+   print('True')
+   print('Correct....')
+   print('-----------')
+
+else :
+    print("False.....")
+    print("incorrect-----------")    
+    
+print("SAU.....")
