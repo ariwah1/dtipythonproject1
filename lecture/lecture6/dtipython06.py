1391e936fed0e6ecd3ef7f6d0bd6a54dc9fe5f6d
ariwah1 <s6652410013@sau.ac.th>
2023-09-12 10:39:56 +0700

    Update dtipython06.py

---
+++ lecture/lecture6/dtipython06.py
@@ -0,0 +1,20 @@
+# คำสั่ง 
+
+for x in range(10) :
+    print(f'DDT-{x}')
+
+print('Han')
+
+print('------------------') 
+
+for y in range (2,5) :
+    print(f'SAU-{y}')
+    
+print('K')
+
+print('-------------')
+
+for z in range (2,5,3) :
+    print(f'Nie-{z}')
+
+print ('Now')
